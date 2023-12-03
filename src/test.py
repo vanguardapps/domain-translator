7e4ddbc5f8ec722f8b8d977bac800ca98de54c71
vanguardapps <rmcclanahan@vanguardapps.io>
2023-12-03 16:39:23 -0500

    update test.py

--- src/test.py
+++ src/test.py
@@ -1,6 +1,3 @@
-from utils import convert_tsv_csv, relative_path
+import torch
 
-convert_tsv_csv(
-    relative_path("data/english-to-spanish.tsv"),
-    relative_path("data/english_to_spanish.csv"),
-)
+print("is cuda available?", torch.cuda.is_available())
