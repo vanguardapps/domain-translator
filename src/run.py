3fda5dcb445f17bb3b810674e8a8b0be1aaf016e
vanguardapps <rmcclanahan@vanguardapps.io>
2023-12-03 01:36:36 -0500

    Use CUDA

--- src/run.py
+++ src/run.py
@@ -93,10 +93,10 @@ def main():
     tokenized_dataset = dataset.map(tokenize_function, batched=True, batch_size=2)
 
     train_args = Seq2SeqTrainingArguments(
-        use_cpu=True,  # Set to False to automatically enable CUDA / mps device
+        use_cpu=False,  # Set to False to automatically enable CUDA / mps device
         per_device_train_batch_size=4,
         per_device_eval_batch_size=4,
-        num_train_epochs=1,
+        num_train_epochs=10,
         save_strategy="epoch",
         evaluation_strategy="epoch",
         fp16=False,
